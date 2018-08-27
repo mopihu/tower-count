7aa4c3f749172f7a9bf2a1c59cb46345670cacc6
m0py <mopihu@gmail.com>
2018-08-27 15:48:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-const Command = require('command')
+module.exports = function TowerCount(mod) {
+  let destroyedTowers = {};
 
-module.exports = function TowerCount(dispatch) {
-  const command = Command(dispatch)
-
-  let destroyedTowers = {}
-
-  dispatch.hook('S_DESTROY_GUILD_TOWER', 1, (event) => {
+  mod.hook('S_DESTROY_GUILD_TOWER', 1, (event) => {
     if (destroyedTowers[event.sourceGuildName]) {
-      destroyedTowers[event.sourceGuildName] += 1
+      destroyedTowers[event.sourceGuildName] += 1;
     } else {
-      destroyedTowers[event.sourceGuildName] = 1
+      destroyedTowers[event.sourceGuildName] = 1;
     }
   })
 
-  command.add('towers', () => {
-    Object.keys(destroyedTowers).sort( (a,b) => destroyedTowers[b] - destroyedTowers[a] ).forEach((guild) => {
-      command.message(guild+': '+destroyedTowers[guild])
-    })
+  mod.command.add('towers', {
+    $default() {
+      mod.command.message('Usage: /8 towers [guild|raid]');
+    },
+    guild() {
+      print(2);
+    },
+    raid() {
+      print(32);
+    },
+    $none() {
+      print();
+    },
   })
 
+  function print(channel) {
+    Object.keys(destroyedTowers).sort((a, b) => destroyedTowers[b] - destroyedTowers[a]).forEach((guild, index) => {
+      let msg = `${guild}: ${destroyedTowers[guild]}`;
+      if (channel) {
+        setTimeout(function() {
+          mod.send('C_CHAT', 1, {
+            channel: channel,
+            message: msg,
+          });
+        }, index * 500);
+      } else {
+        mod.command.message(msg);
+      }
+    })
+  }
+
   this.destructor = function() {
-    command.remove('towers')
+    mod.command.remove('towers');
   }
 }
